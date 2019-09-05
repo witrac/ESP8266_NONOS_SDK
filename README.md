8f6e91d10a47afab36348559e7bd213da8cc5715
Héctor Bravo <hector.bravo@witrac.es>
2019-09-05 17:24:47 +0200

    Modified the example\at_wpa2 formware to add the new AT command AT+SETPEAP="identifier","user","password" that allows to connect to a WPA2 + PEAP using the given credentials. The maximum size for each parameter is 32 characters

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ESP8266_NONOS_SDK
 
 All documentations @ http://espressif.com/en/support/download/documents?keys=&field_type_tid%5B%5D=14
+
+IMPORTANT: To compile one example, move it to the root folder from the examples folder, access the directory and execute gen_misc.sh or to quick compile
+for ESP8266 modules with 8Mbit EEPROM, 40MHz SPI speed, QIO SPI mode, boot v1.2+ and user1.bin output, launch this instead of gen_misc.sh:
+make COMPILE=gcc BOOT=new APP=1 SPI_SPEED=40 SPI_MODE=QIO SPI_SIZE_MAP=2
+
--- examples/at_wpa2/user/user_main.c
+++ examples/at_wpa2/user/user_main.c
@@ -32,12 +32,9 @@
 #include "wpa2_pki/client_crt.h"
 #include "wpa2_pki/client_key.h"
 
-#define SSID "PEAP_ssid"
-#define PASSWORD ""
-
-#define WPA2_IDENTITY "username"
-#define WPA2_USERNAME "PEAP_user"
-#define WPA2_PASSWORD "PEAP_password"
+#define WPA2_MAX_IDENTITY_SIZE 32
+#define WPA2_MAX_USERNAME_SIZE 32
+#define WPA2_MAX_PASSWORD_SIZE 32
 
 typedef enum {
     EAP_TLS,
@@ -45,91 +42,104 @@ typedef enum {
     EAP_TTLS,
 } eap_method_t;
 
-// test :AT+TEST=1,"abc"<,3>
+// test :AT+SETPEAP="identity","username","password"
 void ICACHE_FLASH_ATTR
-at_setupCmdTest(uint8_t id, char *pPara)
+at_setupCmdSetPEAP(uint8_t id, char *pPara)
 {
-    int result = 0, err = 0, flag = 0;
-    uint8 buffer[32] = {0};
+    char identity[WPA2_MAX_IDENTITY_SIZE+1];
+    char username[WPA2_MAX_USERNAME_SIZE+1];
+    char password[WPA2_MAX_IDENTITY_SIZE+1];
+
     pPara++; // skip '='
 
-    //get the first parameter
-    // digit
-    flag = at_get_next_int_dec(&pPara, &result, &err);
-
-    // flag must be ture because there are more parameter
-    if (flag == FALSE) {
+    // get the first parameter
+    memset(identity, 0, sizeof(identity));
+    at_data_str_copy(identity, &pPara, sizeof(identity)-1);
+    if (strlen(identity) == 0 || *pPara++ != ',') {
         at_response_error();
         return;
     }
+    // at_port_print("identity:");
+    // at_port_print(identity);
+    // at_port_print("\r\n");
 
-    if (*pPara++ != ',') { // skip ','
+    // get the second parameter
+    memset(username, 0, sizeof(username));
+    at_data_str_copy(username, &pPara, sizeof(username)-1);
+    if (strlen(username) == 0 || *pPara++ != ',') {
         at_response_error();
         return;
     }
+    // at_port_print("username:");
+    // at_port_print(username);
+    // at_port_print("\r\n");
 
-    os_sprintf(buffer, "the first parameter:%d\r\n", result);
+    // get the third parameter
+    memset(password, 0, sizeof(password));
+    at_data_str_copy(password, &pPara, sizeof(password)-1);
+    if (strlen(password) == 0 || *pPara++ != '\r') {
+        at_response_error();
+        return;
+    }
+    // at_port_print("password:");
+    // at_port_print(password);
+    // at_port_print("\r\n");
+
+
+    at_response_ok();
+
+    eap_method_t method = EAP_PEAP;
+
+    wifi_station_set_wpa2_enterprise_auth(1);
+
+    wifi_station_set_enterprise_identity(identity, os_strlen(identity));//This is an option. If not call this API, the outer identity will be "anonymous@espressif.com".
+
+    if (method == EAP_TLS) {
+        wifi_station_set_enterprise_cert_key(client_cert, os_strlen(client_cert)+1, client_key, os_strlen(client_key)+1, NULL, 1);
+        // wifi_station_set_enterprise_username(username, os_strlen(username));//This is an option for EAP_PEAP and EAP_TLS.
+    }
+    else if (method == EAP_PEAP || method == EAP_TTLS) {
+        wifi_station_set_enterprise_username(username, os_strlen(username));
+        wifi_station_set_enterprise_password(password, os_strlen(password));
+        // wifi_station_set_enterprise_ca_cert(ca, os_strlen(ca)+1);//This is an option for EAP_PEAP and EAP_TTLS.
+    }
+
+    // wifi_station_connect();
+}
+
+void ICACHE_FLASH_ATTR
+at_SetPEAPCmdSetPEAP(uint8_t id)
+{
+    uint8 buffer[32] = {0};
+
+    os_sprintf(buffer, "%s\r\n", "at_SetPEAPCmdSetPEAP");
     at_port_print(buffer);
-
-    //get the second parameter
-    // string
-    at_data_str_copy(buffer, &pPara, 10);
-    at_port_print("the second parameter:");
-    at_port_print(buffer);
-    at_port_print("\r\n");
-
-    if (*pPara == ',') {
-        pPara++; // skip ','
-        result = 0;
-        //there is the third parameter
-        // digit
-        flag = at_get_next_int_dec(&pPara, &result, &err);
-        // we donot care of flag
-        os_sprintf(buffer, "the third parameter:%d\r\n", result);
-        at_port_print(buffer);
-    }
-
-    if (*pPara != '\r') {
-        at_response_error();
-        return;
-    }
-
     at_response_ok();
 }
 
 void ICACHE_FLASH_ATTR
-at_testCmdTest(uint8_t id)
+at_queryCmdSetPEAP(uint8_t id)
 {
     uint8 buffer[32] = {0};
 
-    os_sprintf(buffer, "%s\r\n", "at_testCmdTest");
+    os_sprintf(buffer, "%s\r\n", "at_queryCmdSetPEAP");
     at_port_print(buffer);
     at_response_ok();
 }
 
 void ICACHE_FLASH_ATTR
-at_queryCmdTest(uint8_t id)
+at_exeCmdSetPEAP(uint8_t id)
 {
     uint8 buffer[32] = {0};
 
-    os_sprintf(buffer, "%s\r\n", "at_queryCmdTest");
-    at_port_print(buffer);
-    at_response_ok();
-}
-
-void ICACHE_FLASH_ATTR
-at_exeCmdTest(uint8_t id)
-{
-    uint8 buffer[32] = {0};
-
-    os_sprintf(buffer, "%s\r\n", "at_exeCmdTest");
+    os_sprintf(buffer, "%s\r\n", "at_exeCmdSetPEAP");
     at_port_print(buffer);
     at_response_ok();
 }
 
 extern void at_exeCmdCiupdate(uint8_t id);
 at_funcationType at_custom_cmd[] = {
-    {"+TEST", 5, at_testCmdTest, at_queryCmdTest, at_setupCmdTest, at_exeCmdTest},
+    {"+SETPEAP", 8, at_SetPEAPCmdSetPEAP, at_queryCmdSetPEAP, at_setupCmdSetPEAP, at_exeCmdSetPEAP},
 #ifdef AT_UPGRADE_SUPPORT
     {"+CIUPDATE", 9,       NULL,            NULL,            NULL, at_exeCmdCiupdate}
 #endif
@@ -187,43 +197,10 @@ user_rf_cal_sector_set(void)
     return rf_cal_sec;
 }
 
-void user_set_station_config(void)
-{
-    char ssid[32] = SSID;
-    char password[64] = PASSWORD;
-    struct station_config sta_conf = { 0 };
-
-    os_memcpy(sta_conf.ssid, ssid, 32);
-    os_memcpy(sta_conf.password, password, 64);
-    wifi_station_set_config(&sta_conf);
-}
-
-void user_set_wpa2_config(void)
-{
-    eap_method_t method = EAP_PEAP;
-    char *identity = WPA2_IDENTITY;
-    char *username = WPA2_USERNAME;
-    char *password = WPA2_PASSWORD;
-
-    wifi_station_set_wpa2_enterprise_auth(1);
-
-    //wifi_station_set_enterprise_identity(identity, os_strlen(identity));//This is an option. If not call this API, the outer identity will be "anonymous@espressif.com".
-
-    if (method == EAP_TLS) {
-        wifi_station_set_enterprise_cert_key(client_cert, os_strlen(client_cert)+1, client_key, os_strlen(client_key)+1, NULL, 1);
-        //wifi_station_set_enterprise_username(username, os_strlen(username));//This is an option for EAP_PEAP and EAP_TLS.
-    }
-    else if (method == EAP_PEAP || method == EAP_TTLS) {
-        wifi_station_set_enterprise_username(username, os_strlen(username));
-        wifi_station_set_enterprise_password(password, os_strlen(password));
-        //wifi_station_set_enterprise_ca_cert(ca, os_strlen(ca)+1);//This is an option for EAP_PEAP and EAP_TTLS.
-    }
-}
-
 void ICACHE_FLASH_ATTR
 user_init(void)
 {
-    char buf[128] = {0};
+    // char buf[128] = {0};
     at_customLinkMax = 5;
     at_init();
 // #ifdef ESP_AT_FW_VERSION
@@ -235,14 +212,9 @@ user_init(void)
 // #else
 //     os_sprintf(buf,"compile time:%s %s",__DATE__,__TIME__);
 // #endif
-    at_set_custom_info(buf);
+    // at_set_custom_info(buf);
     at_port_print("\r\nready\r\n");
     at_cmd_array_regist(&at_custom_cmd[0], sizeof(at_custom_cmd)/sizeof(at_custom_cmd[0]));
 
-    os_printf("This is an example for wpa2 enterprise...\n");
-
-    wifi_set_opmode(STATION_MODE);
-    user_set_station_config();
-    user_set_wpa2_config();
-    wifi_station_connect();
+    // wifi_station_connect();
 }
